6eebd9de572c32bfc1159e55cb21b0e45a8cbced
gowthamaraj <gowthamaraj360@gmail.com>
2020-06-03 01:18:29 +0530

    bug: one option removed on search results book.

--- src/search.js
+++ src/search.js
@@ -64,7 +64,6 @@ class Search extends Component {
                           <option value="currentlyReading">Currently Reading</option>
                           <option value="wantToRead">Want to Read</option>
                           <option value="read">Read</option>
-                          <option value="none">None</option>
                         </select>
                       </div>
                     </div>
