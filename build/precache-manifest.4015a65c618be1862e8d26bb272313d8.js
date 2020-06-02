217f8821752128347fb6ca8865834f311fdbfe25
gowthamaraj <gowthamaraj360@gmail.com>
2020-06-03 01:08:09 +0530

    build folder for upload

---
+++ build/precache-manifest.4015a65c618be1862e8d26bb272313d8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345aa3f9bcbde384034645c62b2c6e2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44d4114b3c51a93dd12b",
+    "url": "/static/css/main.1f5591d5.chunk.css"
+  },
+  {
+    "revision": "e96c194e8ddb5b52f65e",
+    "url": "/static/js/2.1d9f2ba3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1d9f2ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d4114b3c51a93dd12b",
+    "url": "/static/js/main.795c7bad.chunk.js"
+  },
+  {
+    "revision": "346b06957f6a7616ca59",
+    "url": "/static/js/runtime-main.a6a3bebb.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
